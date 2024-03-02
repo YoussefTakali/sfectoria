745dab8c1aa2df964a89d346253a362f37a0518c
Youssef Takali <yousseftakali159@gmail.com>
2024-03-02 07:27:56 +0100

    models done

--- src/user/user/user.service.ts
+++ src/user/user/user.service.ts
@@ -1,33 +1,39 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-    constructor(private prisma: PrismaService){}
+  constructor(private prisma: PrismaService) {}
 
-    async addUser(data: User): Promise<User> {
-        return this.prisma.user.create({ data });
-      }
-    
-      async updateUser(id: string, data: User): Promise<User> {
-        const user = await this.prisma.user.update({
-          where: { id },
-          data,
-        });
-        if (!user) {
-          throw new NotFoundException(`User with ID ${id} not found`);
-        }
-        return user;
-      }
-    
-      async removeUser(id: string): Promise<User> {
-        const user = await this.prisma.user.delete({
-          where: { id },
-        });
-        if (!user) {
-          throw new NotFoundException(`User with ID ${id} not found`);
-        }
-        return user;
-      }
+  async addUser(data: User): Promise<User> {
+    return this.prisma.user.create({ data });
+  }
+
+  async hashPassword(password: string): Promise<string> {
+    const saltRounds = 10;
+    return bcrypt.hash(password, saltRounds);
+  }
+
+  async updateUser(id: string, data: User): Promise<User> {
+    const user = await this.prisma.user.update({
+      where: { id },
+      data,
+    });
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
     }
+    return user;
+  }
+
+  async removeUser(id: string): Promise<User> {
+    const user = await this.prisma.user.delete({
+      where: { id },
+    });
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+    return user;
+  }
+}
